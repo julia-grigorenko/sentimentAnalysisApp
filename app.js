704c4f133b6df40846f88956e8220874bb5edeb9
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-06-21 18:29:05 +0200

    connected nlp route to server app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 require('dotenv').config();
+const nlpRouter = require('./routes/nlp');
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/api/nlp', nlpRouter);
 
 
 module.exports = app;
