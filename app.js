67feeb4cfbffb0e87ed2981217cd486392a95f19
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-06-21 10:54:06 +0200

    added basic app functionality

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+module.exports = app;
